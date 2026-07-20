d2c5b2237900cd051b291f9460adeb508924abcb
Stefan <git@stefn.co.uk>
2026-07-20 15:27:44 +0100

    Split storage tests by concern

---
+++ test/shared/storage/fixtures.ts
@@ -0,0 +1,8 @@
+export const STORAGE_TEST_ENV = {
+  encryptionKey: true,
+  env: {
+    LOCAL_STORAGE_PATH: undefined,
+    STORAGE_ZONE_KEY: undefined,
+    STORAGE_ZONE_NAME: undefined,
+  },
+} as const;
