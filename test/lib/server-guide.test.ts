82d2572a74eb8f7375a7d74337a550336c722e7b
Stefan <git@stefn.co.uk>
2026-07-23 09:38:12 +0100

    Render env-sensitive guide test fresh

--- test/lib/server-guide.test.ts
+++ test/lib/server-guide.test.ts
@@ -315,7 +315,7 @@ describeWithEnv("server (admin guide)", { db: true }, () => {
 
     test("hides built sites section when builder is disabled", async () => {
       using _env = withEnv({ CAN_BUILD_SITES: undefined });
-      const html = await guide();
+      const html = await assertAdminHtml("/admin/guide");
       expect(html).not.toContain('id="built-sites"');
     });
 
